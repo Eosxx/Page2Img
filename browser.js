7b6162e00e1fb0ea6a4ebf4e58d0f0f0597f69ed
Eosxx <eospeng@gamil.com>
2020-08-19 15:46:46 +0800

    feature: 支持选择器截图

--- browser.js
+++ browser.js
@@ -36,7 +36,7 @@ class Browser {
    * @return {Buffer}                  图片
    */
   async screenshot ({
-    target, width = 1366, height = 768, fileName
+    target, width = 1366, height = 768, fileName, selector
   }) {
     const browser = this.browser
     const page = await browser.newPage()
@@ -50,9 +50,18 @@ class Browser {
     await page.goto(target, {
       waitUntil: 'networkidle0'
     })
-    const img = await page.screenshot({
+
+    let elm = page
+    let isFullPage = true
+    // 根据选择器选取元素
+    if (selector) {
+      isFullPage = false // 截取部分元素的话，fullPage需要是false
+      elm = await page.$(selector)
+    }
+
+    const img = await elm.screenshot({
       type: 'png', // 即使是质量100的jpeg，也不如png，所以这里建议用png。
-      fullPage: true, // 如果设置为true，则对完整的页面（需要滚动的部分也包含在内）。默认是false
+      fullPage: isFullPage, // 如果设置为true，则对完整的页面（需要滚动的部分也包含在内）。默认是false
       path: `tmp/${fileName}.png`, // 截图保存路径。截图图片类型将从文件扩展名推断出来。如果是相对路径，则从当前路径解析。如果没有指定路径，图片将不会保存到硬盘。
       // quality: 100, // 图片质量, 可选值 0-100. png 类型不适用。
     })
@@ -109,7 +118,7 @@ class Browser {
    * @return {Buffer}                  图片
    */
   async getImg ({
-    target, width, height, queryString
+    target, width, height, queryString, selector
   }) {
     const md5 = MD5(queryString)
 
@@ -135,7 +144,8 @@ class Browser {
         target,
         width,
         height,
-        fileName: md5
+        fileName: md5,
+        selector
       })
     }
   }
--- routes.js
+++ routes.js
@@ -1,19 +1,22 @@
 const Router = require('koa-router')
-const router = new Router()
 const Browser = require('./browser')
 
+const router = new Router()
 const browser = new Browser() // 浏览器对象
 
 // 返回图片
 router.get('/img', async ctx => {
   const query = ctx.query
-
   ctx.type = 'image/png'
+
+
+  const selector = query.s.replace(/@/g, '#')
 
   ctx.body = await browser.getImg({
     target: query.url,
     width: query.w,
     height: query.h,
+    selector: selector,
     queryString: ctx.querystring
   })
 })
